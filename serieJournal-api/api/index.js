3300a8debcf15d5f0cdd41378fc9b0377241457e
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-12-16 23:45:53 -0300

    serveless function path e module 3

--- serieJournal-api/api/index.js
+++ serieJournal-api/api/index.js
@@ -3,14 +3,22 @@ const path = require("path");
 
 module.exports = (req, res) => {
   try {
-    const filePath = path.join(process.cwd(), "data", "series.json");
+    const cwd = process.cwd();
 
-    const file = fs.readFileSync(filePath, "utf8");
-    const data = JSON.parse(file);
+    const files = fs.readdirSync(cwd);
+    const dataFiles = fs.existsSync(path.join(cwd, "data"))
+      ? fs.readdirSync(path.join(cwd, "data"))
+      : "PASTA data NÃO EXISTE";
 
-    res.status(200).json(data);
+    res.status(200).json({
+      cwd,
+      rootFiles: files,
+      dataFolder: dataFiles
+    });
   } catch (error) {
-    console.error("ERROR:", error);
-    res.status(500).json({ error: "Falha em carregar os dados." });
+    res.status(500).json({
+      error: error.message,
+      stack: error.stack
+    });
   }
 };
