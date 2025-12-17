d4ecb979b071cb72b4db8998ec4db2c345a34865
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-12-16 23:43:01 -0300

    serveless function path e module 2

--- serieJournal-api/api/index.js
+++ serieJournal-api/api/index.js
@@ -3,19 +3,14 @@ const path = require("path");
 
 module.exports = (req, res) => {
   try {
-    const filePath = path.join(
-      process.cwd(),
-      "serieJournal-api",
-      "data",
-      "series.json"
-    );
+    const filePath = path.join(process.cwd(), "data", "series.json");
 
     const file = fs.readFileSync(filePath, "utf8");
     const data = JSON.parse(file);
 
     res.status(200).json(data);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to load series data" });
+    console.error("ERROR:", error);
+    res.status(500).json({ error: "Falha em carregar os dados." });
   }
 };
