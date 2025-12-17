b969999f331e2c8042173721da34bafe6c60416e
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-12-16 22:48:37 -0300

    simplify package.json for vercel

--- serieJournal-api/api/index.js
+++ serieJournal-api/api/index.js
@@ -1,3 +1,17 @@
-const app = require("../app");
+import fs from "fs";
+import path from "path";
 
-module.exports = app;
+export default function handler(req, res) {
+  const filePath = path.join(
+    process.cwd(),
+    "serieJournal-api/data/series.json"
+  );
+
+  const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+  if (req.method === "GET") {
+    return res.status(200).json(data);
+  }
+
+  res.status(405).json({ message: "Method not allowed" });
+}
