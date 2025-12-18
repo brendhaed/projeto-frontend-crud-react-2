e0cff4131d8d722a54b4e66e743c8b6a130cec0a
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-12-18 00:00:54 -0300

    adição de series.js

--- serieJournal-api/api/series.js
+++ serieJournal-api/api/series.js
@@ -3,21 +3,15 @@ const path = require("path");
 
 module.exports = (req, res) => {
   try {
-    const filePath = path.join(
-      process.cwd(),
-      "data",
-      "series.json"
-    );
-
+    const filePath = path.join(process.cwd(), "data", "series.json");
     const rawData = fs.readFileSync(filePath, "utf-8");
     const data = JSON.parse(rawData);
 
     res.status(200).json(data);
   } catch (error) {
-    console.error(error);
     res.status(500).json({
       error: "Falha em carregar os dados",
-      details: error.message
+      details: error.message,
     });
   }
 };
