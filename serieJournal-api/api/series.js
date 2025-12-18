6fcd116229c1ca2270dbe2742664fa335c4f457b
Brendha Eduarda Rodrigues <136868276+brendhaed@users.noreply.github.com>
2025-12-18 00:34:11 -0300

    criação de series.js

---
+++ serieJournal-api/api/series.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (req, res) => {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  try {
+    const filePath = path.join(process.cwd(), "data", "series.json");
+
+    const rawData = fs.readFileSync(filePath, "utf-8");
+    const data = JSON.parse(rawData);
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: "Falha em carregar os dados",
+      details: error.message,
+    });
+  }
+};
